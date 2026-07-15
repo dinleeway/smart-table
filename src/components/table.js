d6fb8cdab72323c292e2821b1a1073e1de328014
Liza Efremova <din.lee.way@gmail.com>
2026-07-15 21:55:16 +0300

    1st ready

--- src/components/table.js
+++ src/components/table.js
@@ -10,20 +10,43 @@ import {cloneTemplate} from "../lib/utils.js";
 export function initTable(settings, onAction) {
     const {tableTemplate, rowTemplate, before, after} = settings;
     const root = cloneTemplate(tableTemplate);
-    
     // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+    before.reverse().forEach(templateId => {
+        root[templateId] = cloneTemplate(templateId);
+        root.container.prepend(root[templateId].container);
+    });
+    
+    after.forEach(templateId => {
+        root[templateId] = cloneTemplate(templateId);
+        root.container.append(root[templateId].container);
+    });
+    
 
     // @todo: #1.3 —  обработать события и вызвать onAction()
 
+    root.container.addEventListener('change', () => onAction());
+
+    root.container.addEventListener('reset', () => {
+        setTimeout(onAction);
+    });
+
+    root.container.addEventListener('submit', (e) => {
+        e.preventDefault();
+        onAction(e.submitter);
+    });
+
     const render = (data) => {
         // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+
         const nextRows = data.map((item) => {
-                const row = cloneTemplate(rowTemplate);
-                row.elements.date.textContent = item.date;
-                row.elements.customer.textContent = item.customer;
-                row.elements.seller.textContent = item.seller;
-                row.elements.total.textContent = item.total;
+            const row = cloneTemplate(rowTemplate);
+                Object.keys(item).forEach((key)=>{
+                    if (key in row.elements) {
+                        row.elements[key].textContent = item[key];
+                    }
+                });
                 return row.container;
+            
         });
         root.elements.rows.replaceChildren(...nextRows);
     }
